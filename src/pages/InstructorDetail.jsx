888dcd094fdb250394156f9ed5e1f9a0588459b0
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-07-08 15:30:38 +0300

    added structure

--- src/pages/InstructorDetail.jsx
+++ src/pages/InstructorDetail.jsx
@@ -1,21 +1,19 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
-const InstructorDetail = () => {
-  const navigate= useNavigate();
+  const InstructorDetail = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [inst, setInst] = useState([]);
 
-  const getInstructor = () => {
+  useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => res.json())
       .then((data) => setInst(data))
       .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    getInstructor();
   },[id]);
+
+
 
   return (
     <div className="container text-center">
@@ -28,8 +26,12 @@ const InstructorDetail = () => {
       <h4>{inst.email}</h4>
       <h4>{inst.phone}</h4>
       <div>
-        <button onClick={()=> navigate("/")}   className="btn btn-success me-2">Home</button>
-        <button onClick={()=> navigate(-1)} className="btn btn-warning">Go Back</button>
+        <button onClick={() => navigate("/")} className="btn btn-success me-2">
+          Home
+        </button>
+        <button onClick={() => navigate(-1)} className="btn btn-warning">
+          Go Back
+        </button>
       </div>
     </div>
   );
